21eae4f596c95746befcebdb7657d0e73c81802a
Kate <catherinefarai@gmail.com>
2022-10-04 13:49:13 +0100

    added python_list python_string

---
+++ python_string.py
@@ -0,0 +1,10 @@
+str16 ='catherine'
+str17 = 'welcome back'
+print(str16 + str17) #concatenating two strings
+print(str16 * 3)#how many times would i call you
+print(str16[0:4]) #slicing
+print(str16[5:9])
+print('kate' in str16) #check if kate is in str17
+print('back' in str17) #is back in str17
+print('kate' not in str16)#check if kate is not in str16
+print('come' in str17) #is come not in str17
